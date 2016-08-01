e4bb0f55aad9c3881cbbb6263fba8870afa7e459
AldenHiggins <johnaldenhiggins@gmail.com>
2016-08-01 01:29:46 -0700

    Got rid of the default third person pawn class from the game mode

--- Source/PoseCreator/PoseCreatorGameMode.cpp
+++ Source/PoseCreator/PoseCreatorGameMode.cpp
@@ -6,10 +6,5 @@
 
 APoseCreatorGameMode::APoseCreatorGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+
 }
